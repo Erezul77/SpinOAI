5d20135b6686535d95ca4a364f1404cad18841ee
erezul77 <erezsnz@gmail.com>
2025-06-21 16:56:35 +0300

    Fix spinoLogo type error using Next.js Image component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,10 @@
-import spinoLogo from "../assets/spino_logo.png";
+import Image from "next/image";
+import spinoLogo from "../assets/spino.png";
 
 export default function Header() {
   return (
     <header className="header">
-      <img src={spinoLogo} alt="SpiñO" className="logo" />
+      <Image src={spinoLogo} alt="SpiñO" className="logo" />
       <div className="header-buttons">
         <button>🌐</button>
         <button>?</button>
